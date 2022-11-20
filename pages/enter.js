1ae0a91826ad468c2f213c0628641e2adeb2b564
Niki <jdeejat@gmail.com>
2022-11-20 18:16:03 +0100

    finished username form

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,10 @@
-import { auth, provider, popup } from '../lib/firebase';
+import { auth, provider, db } from '../lib/firebase';
 import { signInWithPopup, signOut } from 'firebase/auth';
-import { useContext } from 'react';
+import { writeBatch, doc, getDoc } from 'firebase/firestore';
+import { useContext, useState, useCallback, useEffect } from 'react';
 import { UserContext } from '../lib/context';
+// lodash import
+import { debounce } from 'lodash';
 
 export default function Enter(props) {
   const { user, username } = useContext(UserContext);
@@ -42,6 +45,121 @@ function SignOutButton() {
   return <button onClick={() => signOut(auth)}>Sign Out</button>;
 }
 
+// Username form
 function UsernameForm() {
-  return null;
+  const [formValue, setFormValue] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { user, username } = useContext(UserContext);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create refs for both documents
+    const userDoc = doc(db, `users/${user.uid}`);
+    const usernameDoc = doc(db, `usernames/${formValue}`);
+
+    // Commit both docs together as a batch write.
+    const batch = writeBatch(db);
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  };
+
+  const onChange = (e) => {
+    // force form value typed in form to match this rule
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+    // only set form value if length is < 3 OR it passes regex
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  // Check if the entered username is valid
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  // Hit the database for username match after each debounced change
+  // useCallback is required for debounce to work
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = doc(db, `usernames/${username}`);
+        const exists = await getDoc(ref);
+
+        console.log('Firestore read executed!');
+
+        if (exists.exists()) {
+          //console.log('Document data:', exists.data());
+          setIsValid(false);
+        } else {
+          // doc.data() will be undefined in this case
+          //console.log('No such document!');
+          setIsValid(true);
+        }
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  return (
+    !username && (
+      <section>
+        <h3>Choose Username</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            name="username"
+            placeholder="myname"
+            value={formValue}
+            onChange={onChange}
+          />
+          <UsernameMessage
+            username={formValue}
+            isValid={isValid}
+            loading={loading}
+          />
+          <button type="submit" className="btn-green" disabled={!isValid}>
+            Choose
+          </button>
+
+          <h3>Debug State</h3>
+          <div>
+            Username: {formValue}
+            <br />
+            Loading: {loading.toString()}
+            <br />
+            Username Valid: {isValid.toString()}
+          </div>
+        </form>
+      </section>
+    )
+  );
+}
+
+function UsernameMessage({ username, isValid, loading }) {
+  if (loading) {
+    return <p>Checking...</p>;
+  } else if (isValid) {
+    return <p className="text-success">{username} is available!</p>;
+  } else if (username && !isValid) {
+    return <p className="text-danger">That username is taken!</p>;
+  } else {
+    return <p></p>;
+  }
 }
