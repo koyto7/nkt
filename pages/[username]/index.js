61ccdea6142ba5e2e59806126c2d46a7d5da9ade
Niki <jdeejat@gmail.com>
2022-11-20 21:56:23 +0100

    finished user page with posts

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,61 @@
-export default function UserPage({}) {
+import { getUserWithUsername, postToJSON } from '../../lib/firebase';
+import { UserProfile } from '../../components/UserProfile';
+import { PostFeed } from '../../components/PostFeed';
+
+import {
+  where,
+  getDocs,
+  orderBy,
+  limit,
+  collection,
+  collectionGroup,
+  query as fQuery,
+} from 'firebase/firestore';
+import { db } from '../../lib/firebase';
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+  console.log(userDoc.ref.path);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+
+    // //alternative to userDoc.ref.path is to search through collection with collectionGroup()
+    // const postsQuery = fQuery(
+    //   collectionGroup(db, 'posts'),
+    //   where('published', '==', true),
+    //   orderBy('createdAt', 'desc'),
+    //   limit(5)
+    // );
+    const postsQuery = fQuery(
+      collection(db, userDoc.ref.path, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      limit(5)
+    );
+
+    // posts = (await getDocs(postsQuery)).docs.map((doc) => doc.data());
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+    console.log(posts);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
   return (
     <main>
-      <h1>User Page</h1>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   );
 }
